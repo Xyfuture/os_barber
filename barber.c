e8dc75d295e7fae3866847ca64546fd4be4e7f66
xyfuture <1728027202@qq.com>
2020-12-06 17:14:18 +0800

    small

--- barber.c
+++ barber.c
@@ -5,29 +5,28 @@ int sleep_time;
 
 void barber()
 {
-    down(call_barber_id);
-    int cid = *consumer_id_shm_content;
+    down(call_barber_id);//有顾客唤醒自己,说明有人来,否则相当于没人,就等待,理发师睡眠
+    int cid = *consumer_id_shm_content;//获取消费者id,这个缓冲区在消费者端进行了互斥,这里就不用了
     printf("barber %d have consumer %d\n",bid,cid);
-    // sleep(sleep_time);
-    *barber_id_shm_content = bid;
-    up(call_consumer_id);
+    *barber_id_shm_content = bid;//传输自己的id
+    up(call_consumer_id);//告诉消费者传输完毕,进行同步
     printf("barber %d is cutting\n",bid);
     sleep(sleep_time);
-    up(wait_cut_finish_id[bid]);
+    up(wait_cut_finish_id[bid]);//告诉消费者理发完成
     printf("barber %d finishes cutting\n",bid);
-    down(bill_cnt_id);
-    *wait_pay_cnt_shm_content +=1;
+    down(bill_cnt_id);//账本只能一个人操作,互斥进行
+    *wait_pay_cnt_shm_content +=1;//理完发没付钱的人数增加
     up(bill_cnt_id);
-    
-    down(bill_cnt_id);
-    while(*wait_pay_cnt_shm_content >0 )
+    //收款阶段
+    down(bill_cnt_id);//账本互斥操作
+    while(*wait_pay_cnt_shm_content >0 )//收钱时,一个理发师将所有等待付钱的人的钱全部收掉
     {
         printf("barber %d receive money\n",bid);
-        up(wait_pay_id);
+        up(wait_pay_id);//收钱,允许一个理发师收多个人的钱
         *wait_pay_cnt_shm_content -=1;
         sleep(sleep_time);
     }
-    up(bill_cnt_id);
+    up(bill_cnt_id);//操作完成,释放账本资源
 }
 
 int main(int argc,char** argv)
--- consumer.c
+++ consumer.c
@@ -7,39 +7,35 @@ int sleep_time;
 void consumer()
 {
     sleep(sleep_time);
-    down(consumer_cnt_mux_id);
+    down(consumer_cnt_mux_id);//检查理发店内人数,临界资源,需要互斥
     if(*consumer_cnt_shm_content <20)
     {
         //进入理发店
-        (*consumer_cnt_shm_content) += 1;
-        up(consumer_cnt_mux_id);
+        (*consumer_cnt_shm_content) += 1;//人员增加
+        up(consumer_cnt_mux_id);//使用结束,释放临界资源
         printf("consumer %d enter waiting room\n",cid);
-        down(sofa_id); // 尝试从等待室进入沙发
+        down(sofa_id); // 尝试从等待室进入沙发,如果等待说明沙发满,相当于在等待室等待
         sleep(sleep_time);
         printf("consumer %d enter sofa room\n",cid);
-        down(cutting_cnt_id); // 看看能不能开始理发
-        //开始理发,能开始理发,首先将沙发释放
-        up(sofa_id);
+        down(cutting_cnt_id); //检查是否能够理发
+        up(sofa_id);//开始理发,能开始理发,首先将沙发释放
         printf("consumer %d ready to get hair cut\n",cid);
-        down(cut_trans_mux_id);// 相互之间互斥传输数据
+        down(cut_trans_mux_id);//首先客户和理发师之间互相使用共享内存交换各自的id,这时访问临界资源,进行互斥
         *consumer_id_shm_content = cid;//告诉理发师自己的id
-        //sleep(sleep_time);
         up(call_barber_id);//唤醒一个理发师
         down(call_consumer_id);//等待理发师传输理发师id
-        int bid = *barber_id_shm_content;
-        up(cut_trans_mux_id);//传输结束
+        int bid = *barber_id_shm_content;//获取理发师id
+        up(cut_trans_mux_id);//传输结束,释放临界资源
         printf("consumer %d find barber %d\n",cid,bid);
         down(wait_cut_finish_id[bid]);//等待理发师结束理发
         //理发结束,开始付款
         printf("consumer %d cutting finish,wait pay for money\n",cid);
-        // down(bill_mux_id);//同一时间只能有一个人付款,开始互斥
-        sleep(sleep_time);
         down(wait_pay_id); // 等待理发师收款
         printf("consumer %d finish payment\n",cid);
-        // up(bill_mux_id);
+
         //此时理发结束,也付完钱了
         up(cutting_cnt_id);//释放一个理发资源
-        down(consumer_cnt_mux_id);
+        down(consumer_cnt_mux_id);//离开理发店,人数-1,互斥访问临界资源
         (*consumer_cnt_shm_content) -=1;
         up(consumer_cnt_mux_id);
         printf("consumer %d leaves barber shop\n",cid);
